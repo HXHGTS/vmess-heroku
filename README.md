069230ff828abb9d979dc5b4c619d0fb6597f12f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-02-14 16:01:56 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# xray Heroku
+
+## 警告⚠：此技术仅限用于个人查看学术或科研娱乐使用！！！若用于其他违法目的，后果自负！！！
+
+## 概述
+
+用于在 Heroku 上部署 xray Websocket + tls
+
+## 一键部署到Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://dashboard.heroku.com/new?template=https://github.com/HXHGTS/xray-heroku)
+
+## ENV 设定
+
+### UUID生成方法
+
+Windows下用cmd执行:
+
+`powershell [guid]::NewGuid() | find /v "Guid" | find /v "--" | find "-"`
+
+Linux下用bash执行:
+
+`uuidgen`
+
+在线生成UUID(推荐):
+
+<a href="http://tool.pfan.cn/guidgen/create?chkbrace=0&chkhyphen=1&chkupper=0&num=1" target="_blank">在线生成UUID</a>
+
+### CDN嵌套方法
+
+[CDN嵌套方法](https://hxhgts.ml/xray-websocket-tls-nginx/cdn.html)
+
+## 注意
+
+部署时一定不要用默认的uuid，记得替换！！！
+
+WebSocket 路径为 `/`
+
+`alterId` 为 `0`
+
+heroku每个月限制550h(约23d)的免费时长，时长使用完会导致服务器离线，下个月自动恢复
+
+
---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Download and install xray
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.2.4/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# xray new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": true
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run xray
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
